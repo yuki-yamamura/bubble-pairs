4b3fe6f9293b7561f669551facce33224c6d76e0
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-12-09 18:38:07 +0900

    chore: use google font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Inter } from '@next/font/google';
 import { Toaster } from 'react-hot-toast';
 
 import type { AppProps } from 'next/app';
@@ -5,16 +6,20 @@ import type { AppProps } from 'next/app';
 import '@unocss/reset/tailwind.css';
 import '@/styles/globals.scss';
 
+const inter = Inter({
+  subsets: ['latin'],
+});
+
 const App = ({ Component, pageProps }: AppProps) => {
   void import('@/mocks/initMocks').then(async ({ initMocks }) => {
     await initMocks();
   });
 
   return (
-    <>
+    <div className={inter.className}>
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </div>
   );
 };
 
