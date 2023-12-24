dba76f3497a0ff9231dba933ebe1640e510c6f02
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-12-25 06:26:31 +0900

    refactor: use `useSWRMutation` hook

--- src/features/members/components/NewMemberForm/index.tsx
+++ src/features/members/components/NewMemberForm/index.tsx
@@ -1,47 +1,48 @@
 import LoadingModal from '@/components/LoadingModal';
 import MemberForm from '@/features/members/components/MemberForm';
-import { useMembers } from '@/features/members/hooks/useMembers';
-import { request } from '@/lib/axios';
+import axios from 'axios';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 import toast from 'react-hot-toast';
+import useSWRMutation from 'swr/mutation';
 
 import type { MemberSchema } from '@/features/members/validation';
 import type { MemberResponseData } from '@/pages/api/member';
-import type { AxiosRequestConfig } from 'axios';
+import type { Prisma } from '@prisma/client';
 
 const NewMemberForm = () => {
-  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
-  const { members, mutate } = useMembers();
+  const fetcher = async (
+    url: string,
+    { arg }: { arg: Prisma.MemberCreateInput },
+  ) => {
+    const response = await axios.post<MemberResponseData>(url, arg);
+
+    return response.data;
+  };
+
+  const { trigger, error, isMutating } = useSWRMutation<
+    MemberResponseData,
+    Error,
+    '/api/member',
+    Prisma.MemberCreateInput
+  >('/api/member', fetcher);
 
   const notifySuccess = () => toast.success('メンバーを登録しました。');
   const notifyError = () => toast.error('メンバーの登録に失敗しました。');
   const handleSubmit = async (memberCreateInput: MemberSchema) => {
-    setIsLoading(true);
-
-    const axiosRequestConfig: AxiosRequestConfig = {
-      url: '/api/member',
-      method: 'POST',
-      data: {
-        avatar: 'https://picsum.photos/200/300.jpg?random=1',
-        ...memberCreateInput,
-      },
-    };
-    const result = await request<MemberResponseData>(axiosRequestConfig);
-
-    if (result.type === 'success') {
-      notifySuccess();
-      await mutate({ members: [...members, result.data.member] });
-      await router.push('/members');
-    } else {
+    await trigger({
+      ...memberCreateInput,
+      avatar: 'https://picsum.photos/200/300.jpg?random=1',
+    });
+    if (error) {
       notifyError();
+    } else {
+      notifySuccess();
+      await router.push('/members');
     }
-
-    setIsLoading(false);
   };
 
-  if (isLoading) {
+  if (isMutating) {
     return <LoadingModal />;
   }
 
--- src/types/HttpResult.ts
+++
@@ -1,5 +0,0 @@
-import type { Result } from '@/types/Result';
-
-export type HttpResult<T = unknown> = Result<T> & {
-  status: number | undefined;
-};
