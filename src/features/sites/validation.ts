45a9c7a4089cef162873bfe4d5b77588cdf5f6eb
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-01-02 11:58:24 +0900

    chore: use zod omit instead of extend

--- src/features/sites/validation.ts
+++ src/features/sites/validation.ts
@@ -3,20 +3,21 @@ import { z } from 'zod';
 
 import type { Site } from '@prisma/client';
 
-export const siteFormSchema = schemaForType<
-  Omit<Site, 'id' | 'createdAt' | 'updatedAt'>
->()(
+export const siteSchema = schemaForType<Site>()(
   z.object({
+    id: z.number(),
+    createdAt: z.date(),
+    updatedAt: z.date(),
     name: z.string().min(1, '名前を入力してください。'),
     courtCount: z.number(),
     isDefault: z.boolean(),
   }),
 );
 
-export const siteSchema = siteFormSchema.extend({
-  id: z.number(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
+export const siteFormSchema = siteSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
 });
 
 export type SiteFormType = z.infer<typeof siteFormSchema>;
