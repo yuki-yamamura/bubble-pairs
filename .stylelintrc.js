5366597c0b9129a4315813a86a79a77b59fb703a
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-01-31 18:34:19 +0900

    chore: modify Stylelint config for Tailwind CSS

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,13 +1,19 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-standard-scss',
-    'stylelint-config-recess-order',
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   plugins: ['stylelint-order'],
   ignoreFiles: ['**/node_modules/**'],
   rules: {
-    'selector-class-pattern': '^[a-z][a-zA-Z0-9]+$',
-    'keyframes-name-pattern': '^[a-z][a-zA-Z0-9]+$',
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
   },
 };
