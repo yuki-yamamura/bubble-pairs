d5d3b1222e7d7183061da62bf9aa422e7d2cc11d
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-02-01 07:28:34 +0900

    chore: add comment about extended session interface

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,6 +1,10 @@
 import type { DefaultSession } from 'next-auth';
 
 declare module 'next-auth' {
+  /**
+   * An interface to extend the default session so that it includes user id.
+   * see: https://next-auth.js.org/getting-started/typescript#extend-default-interface-properties
+   */
   interface Session {
     user: {
       id: string;
