c18ea0aa5b734f30da024d3898ad34cec4077f9d
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-02-03 17:25:37 +0900

    chore: configure Superjson to serialize date in the server side

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   // todo: set strict mode true when msw's rendering problem is fixed.
   reactStrictMode: false,
+  experimental: {
+    swcPlugins: [
+      [
+        'next-superjson-plugin',
+        {
+          excluded: [],
+        },
+      ],
+    ],
+  },
 };
 
 module.exports = nextConfig;
