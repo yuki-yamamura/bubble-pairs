f55cf7940fe50715f519054cb0545c0dbde24d40
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-02-13 03:54:05 +0900

    fix: fix usage about `cn` utility function

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,11 +6,11 @@ type Props = React.ComponentPropsWithoutRef<typeof ButtonPrimitive> & {
   isBusy?: boolean;
 };
 
-const Button = ({ isBusy, children, ...rest }: Props) => (
+const Button = ({ isBusy, className, children, ...rest }: Props) => (
   <ButtonPrimitive
     disabled={isBusy}
     aria-busy={isBusy}
-    className={cn(['inline-flex gap-x-1 rounded-3xl py-3'])}
+    className={cn('inline-flex gap-x-1 rounded-3xl py-3', className)}
     {...rest}
   >
     {isBusy && <Loader2 size={16} className="animate-spin text-slate-300" />}
