b5a7062312a2a76c6245a06d738bb64e2ccd6ab7
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-02-16 21:35:22 +0900

    chore: remove unused screens and utilities

--- src/screens/NewPlaceScreen/index.tsx
+++
@@ -1,5 +0,0 @@
-import NewPlaceForm from '@/features/places/components/NewPlaceForm';
-
-const NewPlaceScreen = () => <NewPlaceForm />;
-
-export default NewPlaceScreen;
--- src/utils/types.ts
+++
@@ -1,8 +0,0 @@
-import type { SuccessResult } from '@/types/Result';
-
-export type ReturnTypeOf<T extends (...args: never) => Promise<unknown>> =
-  T extends (...args: never) => Promise<infer U>
-    ? U extends SuccessResult<infer D>
-      ? D
-      : never
-    : never;
