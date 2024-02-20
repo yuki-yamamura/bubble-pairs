633f0756d7d29d364607498d9629d24f720ae30c
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-02-21 03:43:22 +0900

    chore: remove unused file

--- src/features/members/types/SortKey.ts
+++
@@ -1,7 +0,0 @@
-import type { Member } from '@prisma/client';
-
-export type SortKey = keyof Pick<
-  Member,
-  // 'createdAt' | 'displayName' | 'sex' | 'level'
-  'createdAt' | 'sex' | 'level'
->;
