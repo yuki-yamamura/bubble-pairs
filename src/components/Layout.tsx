8d81fb3b08410c26f63d9c6ab2dfb5fa7fa5b89e
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-03-02 09:43:46 +0900

    chore: add title tag

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,14 @@
 import Header from '@/components/Header';
+import Head from 'next/head';
 
 type Props = React.PropsWithChildren;
 
 const Layout = ({ children }: Props) => {
   return (
     <div className="mx-auto flex min-h-screen w-full max-w-screen-lg flex-col px-4 pb-32 sm:px-8">
+      <Head>
+        <title>Bubble Pairs</title>
+      </Head>
       <Header />
       <div className="flex grow flex-col">{children}</div>
     </div>
