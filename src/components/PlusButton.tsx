3e4f189f270c416a2a35adb37d2ef30d6c98875b
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-03-10 08:10:29 +0900

    chore: adjust position of plus button for mobile devices

--- src/components/PlusButton.tsx
+++ src/components/PlusButton.tsx
@@ -9,7 +9,7 @@ const PlusButton = ({ onClick }: Props) => (
     type="button"
     onClick={onClick}
     variant="outline"
-    className="fixed bottom-32 left-8 z-50 h-10 w-10 rounded-full p-0 shadow-sm lg:left-32"
+    className="fixed bottom-12 left-8 z-50 h-10 w-10 rounded-full p-0 shadow-sm sm:bottom-32 lg:left-32"
   >
     <PlusIcon size={16} className="text-slate-400" />
   </Button>
