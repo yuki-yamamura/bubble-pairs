7c8d0056fd9e8992aa1cd49fe9b9126ce9a75c7d
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-03-10 13:46:38 +0900

    fix: add missing button for deleting account

--- src/features/users/components/Profile.tsx
+++ src/features/users/components/Profile.tsx
@@ -1,3 +1,4 @@
+import DeleteUserButton from './DeleteUserButton';
 import EditNameSection from './EditNameSection';
 import { useUser } from '@/context/useUser';
 import EditEmailSection from '@/features/users/components/EditEmailSection';
@@ -13,6 +14,9 @@ const Profile = () => {
       <div className="flex flex-col gap-y-12">
         <EditEmailSection title="現在のメールアドレス" user={user} />
         <EditNameSection title="表示名" user={user} />
+        <div className="mt-20 self-center">
+          <DeleteUserButton user={user} />
+        </div>
       </div>
     </div>
   );
