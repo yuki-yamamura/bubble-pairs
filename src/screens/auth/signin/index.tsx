dc32d768b8257e246849d8d77a8b7decd3c27622
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-03-10 11:52:08 +0900

    chore: adjust design on sign in page

--- src/screens/auth/signin/index.tsx
+++ src/screens/auth/signin/index.tsx
@@ -2,9 +2,7 @@ import SignInForm from '@/components/SignInForm';
 
 const SignInScreen = () => (
   <div className="flex h-full flex-col items-center justify-center">
-    <div className="-translate-y-1/2">
-      <SignInForm />
-    </div>
+    <SignInForm />
   </div>
 );
 
