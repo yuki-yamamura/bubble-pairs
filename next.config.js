43650e079fb3ec381472d4e185abb6be756a2898
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-03-13 06:36:21 +0900

    chore: remove unused config for Next.js

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   // todo: set strict mode true when msw's rendering problem is fixed.
   reactStrictMode: false,
-  experimental: {
-    swcPlugins: [
-      [
-        'next-superjson-plugin',
-        {
-          excluded: [],
-        },
-      ],
-    ],
-  },
   redirects: () => {
     return [
       {
