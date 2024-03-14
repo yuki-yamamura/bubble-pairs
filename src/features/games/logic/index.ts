715bcb0a7126f91c89c2073e0b680d5bf82186e8
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-03-15 05:41:21 +0900

    fix: make sure to pick players at random when creating a game

--- src/features/games/logic/index.ts
+++ src/features/games/logic/index.ts
@@ -36,16 +36,53 @@ export const generateGameInput = (
     )
     .filter((participant): participant is Participant => !!participant);
   // sort candidates by the number of games that his/her has played for equality.
-  const sortedCandidates = calculateTotalGamesPerParticipant(
+  // and pick the players at random if their number of games are the same.
+  const sortedCandidates: Participant[] = calculateTotalGamesPerParticipant(
     candidates,
     activity,
   )
-    .sort((a, b) => (a.totalGames > b.totalGames ? 1 : -1))
-    .map(({ participant }) => participant);
+    // make groups of candidates per total game count.
+    .reduce(
+      (
+        candidateGroups: {
+          totalGames: number;
+          candidates: Participant[];
+        }[],
+        currentCandidate,
+      ) => {
+        const isNewGroupCandidate = candidateGroups.every(
+          (group) => group.totalGames !== currentCandidate.totalGames,
+        );
+        if (isNewGroupCandidate) {
+          return [
+            ...candidateGroups,
+            {
+              totalGames: currentCandidate.totalGames,
+              candidates: [currentCandidate.participant],
+            },
+          ];
+        } else {
+          return candidateGroups.map((group) => {
+            if (group.totalGames !== currentCandidate.totalGames) {
+              return group;
+            }
 
-  // pick players depending on how many the game needs players,
-  //   and then shuffle them to make pairs random.
-  const players = shuffleArray(sortedCandidates.slice(0, totalPlayers));
+            return {
+              ...group,
+              candidates: [...group.candidates, currentCandidate.participant],
+            };
+          });
+        }
+      },
+      [],
+    )
+    // sort groups by total game count.
+    .sort((a, b) => (a.totalGames > b.totalGames ? 1 : -1))
+    // make candidates random within the group.
+    .map((group) => shuffleArray(group.candidates))
+    .flat();
+
+  const players = sortedCandidates.slice(0, totalPlayers);
   const resters = sortedCandidates.slice(totalPlayers);
 
   const gameDetails = generateGameDetailInputs(
