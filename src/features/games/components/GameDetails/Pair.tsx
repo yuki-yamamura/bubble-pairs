1d52cff22c0c75da4d8b4e3b4932519aababa05e
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-03-16 05:56:16 +0900

    fix: fix space between two players

--- src/features/games/components/GameDetails/Pair.tsx
+++ src/features/games/components/GameDetails/Pair.tsx
@@ -1,14 +1,13 @@
 import DecoratedMember from '@/features/members/components/DecoratedMember';
-import { cn } from '@/lib/shadcn-ui';
 
 import type { Player } from '@/types/models/Player';
 
-type Props = React.HTMLAttributes<HTMLDivElement> & {
+type Props = {
   players: [Player, Player];
 };
 
-const Pair = ({ players, className, ...rest }: Props) => (
-  <div className={(cn('flex max-w-fit flex-col gap-y-2'), className)} {...rest}>
+const Pair = ({ players }: Props) => (
+  <div className={'flex max-w-fit flex-col gap-y-2'}>
     <DecoratedMember member={players[0].participant.member} />
     <DecoratedMember member={players[1].participant.member} />
   </div>
